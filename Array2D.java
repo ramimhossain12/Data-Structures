66e831e183bd1b09cfdc18ec9e6d180241906679
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-13 21:28:42 +0600

    2D Array

--- Array2D.java
+++ Array2D.java
@@ -1,8 +1,24 @@
 package Ramim_Apps_Ltd.company.ff;
 
-public class Array2D {
+public class  Array2D {
 
     public static void main(String[] args) {
-        System.out.println("Ra,");
+
+
+        int[][] num = new int[2][3];
+
+        num[0][0] = 10;
+        num[0][1] = 20;
+        num[0][2] = 30;
+        num[1][0] = 40;
+        num[1][1] = 50;
+        num[1][2] = 60;
+
+        for (int row=0;row<2;row++){
+            for (int col=0;col<3;col++){
+                System.out.print(" "+num[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
