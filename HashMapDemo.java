c929d7f3d711dc9b3cf19e9d5795532717d2a27c
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-18 00:50:51 +0600

    HashMap

---
+++ HashMapDemo.java
@@ -0,0 +1,31 @@
+package Ramim_Apps_Ltd.company.ff;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+
+        // Create a HashMap object called people
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+
+
+        // Add keys and values (Name, Age)
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+
+        //remove
+        people.remove("Steve");
+
+        //  for clear
+        people.clear();
+
+        // for size
+        people.size();
+
+        for (String i : people.keySet()) {
+            System.out.println("key: " + i + " value: " + people.get(i));
+        }
+    }
+}
