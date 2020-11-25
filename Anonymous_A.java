1219c1778d9c36690fe89fa3b646ad6017579a97
ramimhossain12 <ramimhossain6242@gmail.com>
2020-11-25 12:43:12 +0600

    Anonymous Array

---
+++ Anonymous_A.java
@@ -0,0 +1,31 @@
+
+/*
+  
+    Programmed by: Ramim Hossain
+    Contact: https://ramimhossain.com/
+*/
+
+
+
+package Ramim_Apps_Ltd.company.ff;
+
+public class Anonymous_A {
+
+    void  display(){
+        System.out.println("Person class");
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Anonymous_A a = new Anonymous_A(){
+          void  display()
+          {
+              System.out.println("Test class");
+          }
+        };
+        a.display();
+    }
+}
