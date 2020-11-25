163e5b9a88a0fca5b99cf953761da7d265169c36
ramimhossain12 <ramimhossain6242@gmail.com>
2020-11-25 12:50:03 +0600

    Hash set Demo

---
+++ HashSetDemo.java
@@ -0,0 +1,41 @@
+/*
+  
+    Programmed by: Ramim Hossain
+    Contact: https://ramimhossain.com/
+*/
+
+
+
+package Ramim_Apps_Ltd.company.ff;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+        HashSet<String > fruitsName = new HashSet<>();
+
+     // value add ;
+        fruitsName.add("Apple");
+        fruitsName.add("Orange");
+        fruitsName.add("Mango");
+        fruitsName.add("Guava");
+
+
+        System.out.println(fruitsName);
+        //size
+        System.out.println("size" + fruitsName.size());
+
+        //remove
+        fruitsName.remove("Apple");
+        System.out.println(fruitsName);
+
+        //clear
+        fruitsName.clear();
+        System.out.println(fruitsName);
+
+        //IsEmpty
+        boolean val = fruitsName.isEmpty();
+        System.out.println(val);
+    }
+}
