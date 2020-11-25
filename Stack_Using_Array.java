1dd453555679580da62ccf2b3ab36e28d522af89
ramimhossain12 <ramimhossain6242@gmail.com>
2020-11-25 12:55:28 +0600

    Stack useing array

---
+++ Stack_Using_Array.java
@@ -0,0 +1,65 @@
+/*
+  
+    Programmed by: Ramim Hossain
+    Contact: https://ramimhossain.com/
+*/
+
+
+
+package Ramim_Apps_Ltd.company.ff;
+
+public class Stack_Using_Array {
+
+
+    int capacity =3 ;
+    int[] stack = new int[capacity];
+    int top = -1;
+
+
+    //for push
+    void  push(int x){
+        if (top <capacity-1){
+            top = top+1;
+            stack[top]=x;
+            System.out.println("Successfully added"+ x);
+        }
+        else {
+            System.out.println("Exception! Stack Overflow");
+        }
+
+
+    }
+    //for pop
+    int pop(){
+
+        if(top<0){
+            System.out.println("Exception! Stack Underflow");
+            return  -1;
+        }
+        int val  = stack[top];
+        top = top-1;
+        return val;
+    }
+    //for peek
+    int peek(){
+
+        if(top<0){
+            System.out.println("Exception! Stack Underflow");
+            return  -1;
+        }
+      return stack[top];
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Implementing My Stack ");
+        Stack_Using_Array obj = new Stack_Using_Array();
+        System.out.println("Peek Returned:"+obj.peek());
+        obj.push(30);
+        obj.push(20);
+        obj.push(10);
+        System.out.println("Pop Deleted:"+obj.pop());
+        obj.push(90);
+        System.out.println("Peek Returned:"+obj.peek());
+    }
+}
